5c2ae7256dbf7e678c0b027c2aff94844da576ef
“Kenneth <kenkariuki63@gmail.com>
2022-11-23 23:33:16 -0800

    PRINTF

--- _write_char.c
+++ _write_char.c
@@ -5,7 +5,7 @@
 /**
  * _write_char - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1, on error, -1 is returned, and error is set appropriately
+ * Return: On success 1, on error, -1 is returned
  */
 
 int _write_char(char c)
